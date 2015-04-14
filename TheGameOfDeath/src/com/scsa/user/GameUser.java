0a8688b820511d641836d12e26ba391d957aefbb
spring1514 <spring1514@naver.com>
2015-04-14 14:49:04 +0900

    ㄴㄴ

--- TheGameOfDeath/src/com/scsa/user/GameUser.java
+++ TheGameOfDeath/src/com/scsa/user/GameUser.java
@@ -9,61 +9,86 @@ public class GameUser {
 	private String Nick;
 	private String Pass;
 	private int Play;
+	private String image;
+
 	
 	public GameUser(int userNum, String userId, String nick, String pass,
-			int play) {
+			int play, String image) {
 		super();
 		UserNum = userNum;
 		UserId = userId;
 		Nick = nick;
 		Pass = pass;
 		Play = play;
+		this.image = image;
 	}
+
 
 	public GameUser() {
 		super();
 	}
 
 	
+
 	public int getUserNum() {
 		return UserNum;
 	}
+
 
 	public void setUserNum(int userNum) {
 		UserNum = userNum;
 	}
 
+
 	public String getUserId() {
 		return UserId;
 	}
+
 
 	public void setUserId(String userId) {
 		UserId = userId;
 	}
 
+
 	public String getNick() {
 		return Nick;
 	}
+
 
 	public void setNick(String nick) {
 		Nick = nick;
 	}
 
+
 	public String getPass() {
 		return Pass;
 	}
+
 
 	public void setPass(String pass) {
 		Pass = pass;
 	}
 
+
 	public int getPlay() {
 		return Play;
 	}
 
+
 	public void setPlay(int play) {
 		Play = play;
 	}
+
+
+	public String getImage() {
+		return image;
+	}
+
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
 
 	@Override
 	public String toString() {
@@ -78,9 +103,11 @@ public class GameUser {
 		builder.append(Pass);
 		builder.append(", Play=");
 		builder.append(Play);
+		builder.append(", image=");
+		builder.append(image);
 		builder.append("]");
 		return builder.toString();
 	}
 	
-
+	
 } //class
