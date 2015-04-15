a4514047ebb4ae5683bc923b57fafbd194492a0a
spring1514 <spring1514@naver.com>
2015-04-15 19:19:07 +0900

    ss

--- TheGameOfDeath/src/com/scsa/game/SurvivorServlet.java
+++ TheGameOfDeath/src/com/scsa/game/SurvivorServlet.java
@@ -1,47 +1,59 @@
 package com.scsa.game;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.scsa.user.Status;
 
 public class SurvivorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	GameDao dao = new GameDao(); // 멤버변수 불러서
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
-		
+
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
 		request.setCharacterEncoding("utf-8");
 		String action = request.getParameter("action");
 		System.out.println(action);
 		String nextPage = "";
-		
-		switch(action){
-		case "LIST" : nextPage = list(request, response); break;
-		}//스위치문
-		request.getRequestDispatcher(nextPage).forward(request, response); ///????
+		if (action == null) {
+			nextPage = list(request, response);
+		} else {
+
+			switch (action) {
+			case "LIST":
+				nextPage = list(request, response);
+				break;
+			}// 스위치문
+		}// else
+		request.getRequestDispatcher(nextPage).forward(request, response); // /????
 	}
 
 	private String list(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<Status> list = null; // 리스트를 꺼내준다.
+		try {
+			list = dao.getUserList();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			request.setAttribute("msg", "생존자 검색중 오류가 발생했습니다.");
+			return "Error.jsp";
+		}
+		request.setAttribute("list", list);
+		return "Survivor.jsp";
 	}
 
-	
-	
-	
-	
-	
-	
-} //class
+} // class
 
 /*
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	}
+ * protected void doGet(HttpServletRequest request, HttpServletResponse
+ * response) throws ServletException, IOException { } protected void
+ * doPost(HttpServletRequest request, HttpServletResponse response) throws
+ * ServletException, IOException { }
  */
